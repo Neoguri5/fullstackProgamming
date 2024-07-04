12cbb7e6ee90c71719412f27fc6878947a610ad5
Neoguri5 <lovelamb07@khu.ac.kr>
2024-07-04 18:34:27 +0900

    Add files via upload

---
+++ frontend/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'movie_list.dart'; // MovieList 클래스를 임포트
+import 'upcoming_movies.dart'; // HollywoodSection 클래스를 임포트
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('예매율 TOP3 및 할리우드 기대작'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '오늘의 예매율 TOP3',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            MovieList(), // 예매율 TOP3 섹션
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+              child: Text(
+                '할리우드 기대작',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            HollywoodSection(), // 새로 추가한 섹션
+          ],
+        ),
+      ),
+    );
+  }
+}
