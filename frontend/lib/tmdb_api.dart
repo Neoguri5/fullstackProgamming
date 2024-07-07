b1f983940067723fddcdb0741ebf285ff6ad4b1c
Neoguri5 <lovelamb07@khu.ac.kr>
2024-07-07 21:32:19 +0900

    Add files via upload

--- frontend/lib/tmdb_api.dart
+++ frontend/lib/tmdb_api.dart
@@ -31,33 +31,6 @@ class TMDBApi {
     }
   }
 
-  // 한국 영화 가져오기
-  Future<List<dynamic>> fetchUpcomingKoreanMovies() async {
-    final now = DateTime.now();
-    final sixMonthsLater = DateTime(now.year, now.month + 6, now.day);
-    final String startDate = now.toIso8601String().substring(0, 10);
-    final String endDate = sixMonthsLater.toIso8601String().substring(0, 10);
-
-    final url = 'https://api.themoviedb.org/3/discover/movie?'
-        'api_key=$apiKey&language=ko-KR&sort_by=popularity.desc&'
-        'primary_release_date.gte=$startDate&primary_release_date.lte=$endDate&'
-        'with_original_language=ko';
-
-    final response = await http.get(Uri.parse(url));
-
-    print('API URL: $url'); // 디버깅을 위한 로그 출력
-    if (response.statusCode == 200) {
-      final List<dynamic> movies = json.decode(response.body)['results'];
-      movies.sort((a, b) =>
-          a['release_date'].compareTo(b['release_date'])); // 개봉일 기준으로 정렬
-      return movies;
-    } else {
-      print(
-          'Failed to load upcoming Korean movies: ${response.statusCode}'); // 디버깅을 위한 로그 출력
-      throw Exception('Failed to load upcoming Korean movies');
-    }
-  }
-
   Future<Map<String, dynamic>> fetchMovieDetails(int movieId) async {
     final response = await http.get(Uri.parse(
         'https://api.themoviedb.org/3/movie/$movieId?api_key=$apiKey&language=ko-KR'));
--- frontend/lib/upcoming_kr_mv.dart
+++ frontend/lib/upcoming_kr_mv.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'tmdb_api.dart'; // TMDB API 클래스 임포트
+import 'kofic_api.dart'; // 영화진흥원 API 클래스 임포트
 
 class KoreanSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<dynamic>>(
-      future: TMDBApi().fetchUpcomingKoreanMovies(),
+      future: KOFICApi().fetchUpcomingKoreanMovies(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
@@ -26,25 +26,29 @@ class KoreanSection extends StatelessWidget {
               itemCount: movies.length,
               itemBuilder: (context, index) {
                 final movie = movies[index];
+                final posterPath = movie['poster_path'] != ''
+                    ? 'https://image.tmdb.org/t/p/w500${movie['poster_path']}'
+                    : 'https://via.placeholder.com/150';
+
                 return Container(
                   width: 160.0,
                   margin: EdgeInsets.all(8.0),
                   child: Column(
                     children: [
                       Image.network(
-                        'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
+                        posterPath, // 포스터 URL
                         height: 180,
                         fit: BoxFit.cover,
                       ),
                       SizedBox(height: 8),
                       Text(
-                        movie['title'], // 한국어 제목 사용
+                        movie['movieNm'], // 한국어 제목 사용
                         style: TextStyle(
                             fontSize: 16, fontWeight: FontWeight.bold),
                         overflow: TextOverflow.ellipsis,
                       ),
                       Text(
-                        '개봉일: ${movie['release_date']}',
+                        '개봉일: ${movie['openDt']}',
                         style: TextStyle(fontSize: 14),
                       ),
                     ],
